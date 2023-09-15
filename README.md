f210aeef407f147ec7638ba3766031c7befeb132
b0n3yard <namesake39@gmail.com>
2023-09-15 12:17:47 -0400

    decoration mostly finished, cleaning up

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# 04_module_4
+# Web Design Assessment 
+
+# Description
+This project focused on getting a new portfolio up and running from scratch. The tricky part was the use of positioning and responsive design. 
+
+## Installation
+to insall this project, clone the repository from [github](https://github.com/b0n3yard/Michael_Klein_Portfolio) and open index.html in the chrome browser. alternatively you can view the finished page [here](https://b0n3yard.github.io/Michael_Klein_Portfolio/)
+
+
+## Usage
+![Alt text]( ./assets/Michael-Klein-Portfolio.png "example")
+
+
+## Credits
+Michael Klein
+and a lot of help from [W3schools](https://www.w3schools.com/js/default.asp)
+
+## Licence
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,18 +7,14 @@ var score = 0;
 var wrbtn = document.querySelectorAll('.wrbtn');
 var wbtn1 = document.querySelector('.wans1');
 var wbtn2 = document.querySelector('.wans2');
-console.log(wrbtn)
 var wbtn3 = document.querySelector('.wans3');
 var rbtn = document.querySelector('.rans1');
-var totaltime = 300;
+var totaltime = 20;
 var len = '';
 var end = document.querySelector('.end');
-
+// var lsnames = '';
 var sbbtn = document.querySelector('#sub')
-function exam() {
 
-
-}
 function wrong() {
     count = count + 1;
 
@@ -32,17 +28,19 @@ function wrong() {
     return count;
 }
 function right(count) {
-    console.log(count);
-    console.log(player.score)
     count = count + 1;
     player.score = player.score + 1;
-    console.log(count);
-    console.log("right");
     document.querySelector('.score').innerHTML = player.score;
     var correct = 'correct';
     document.querySelector('.right').innerHTML = correct;
-
-    nextquest(count);
+    if(count >= questions.length){
+        endgame();
+        tstfnc();
+    }else{
+        nextquest(count);
+    }
+    
+    
     return count;
 
 
@@ -50,7 +48,6 @@ function right(count) {
 var questions = new Array('HTML stands for', 'WWW stands for', 'HTTP stands for');
 
 function nextquest(count) {
-    // var questions = new Array('HTML stands for', 'WWW stands for', 'HTTP stands for');
     var wans1 = new Array('Hypo text made up language', 'World Wide Weapon', 'Hyper text Traditional Protocol');
     var wans2 = new Array('Heuristic language', 'World Wide Windows', ' Hyper Text Transfer Program');
     var wans3 = new Array('Hypertext Manipulation language', 'World Wide Writers', 'Hypertext Transmission Protocol');
@@ -66,10 +63,23 @@ var btn = document.querySelector('#generate')
 function show() {
     document.querySelector('.butn').style.display = 'none';
     document.querySelector('.hidden').style.display = 'block';
+    // var timer = setInterval(function () {
+    //     totaltime--
+    //     var minutes = Math.floor(totaltime / 60);
+    //     var secs = totaltime % 60;
+    //     if (count >= questions.length || totaltime <= 0) {
+    //         clearInterval(timer);
+    //         endgame();
+
+    //     }
+    //     document.querySelector('.timer').innerHTML = minutes + ':' + secs;
+    // }, 1000);
+    tstfnc();
+    nextquest(count);
+}
+function tstfnc(){
     var timer = setInterval(function () {
         totaltime--
-        console.log("totaltime: " + totaltime)
-        console.log("cont: " + count)
         var minutes = Math.floor(totaltime / 60);
         var secs = totaltime % 60;
         if (count >= questions.length || totaltime <= 0) {
@@ -80,22 +90,19 @@ function show() {
         document.querySelector('.timer').innerHTML = minutes + ':' + secs;
     }, 1000);
 
-    nextquest(count);
 }
 function gtinfo(e) {
     e.preventDefault()
     len = document.querySelector('#len').value;
-    console.log(len)
     player.name = len;
     var tweb = 'your score is: ' + player.score;
     document.querySelector('.end p').innerHTML = tweb;
+    lsnames = localStorage.getItem('playername');
+    lsscores = localStorage.getItem('playerscore');
     if (lsnames === null) {
         localStorage.setItem('playername', player.name);
         localStorage.setItem('playerscore', player.score);
     }
-    lsnames = localStorage.getItem('playername');
-    lsscores = localStorage.getItem('playerscore');
-    console.log(lsnames + ': ' + lsscores);
     cleanup();
 
 }
@@ -103,43 +110,21 @@ function endgame(timer) {
 
     document.querySelector('.hidden').style.display = 'none';
     document.querySelector('.end').style.display = 'block';
-    // var len = document.querySelector('#len');
-    console.log("hi");
-
-    // player.name =  prompt('enter name');
-    // var tweb = 'your score is: ' + player.score;
-
-    //  document.querySelector('.end p').innerHTML = tweb;
-    // gtinfo();
-
-    // var strinfo = info.toString();
-    // console.log("stringinfo: " + strinfo);
-    // localStorage.setItem('playername', player.name);
-    // localStorage.setItem('playerscore', player.score);
-    // lsnames = localStorage.getItem('playername');
-    // lsscores = localStorage.getItem('playerscore');
-    // console.log(lsnames + ': '+ lsscores);
 
 }
+
 function cleanup() {
     document.querySelector('.end').style.display = 'none';
     document.querySelector('.final').style.display = 'block';
-
-    document.querySelector('#cscore').innerText = player.name + " score: " + player.score;
-    document.querySelector('#last').innerText = lsnames + " score: " + lsscores;
+    document.querySelector('#cscore').innerText = player.name + ": " + player.score;
+    document.querySelector('#last').innerText = lsnames + ": " + lsscores;
     localStorage.setItem('playername', player.name);
     localStorage.setItem('playerscore', player.score);
-    console.log("pname " + player.name)
-    console.log("pscore" + player.score)
-    console.log("lname " + lsnames)
-    console.log("lscore " + lsscores)
-
-
 }
 
 
 btn.addEventListener('click', show)
-rbtn.addEventListener('click', function () { console.log("main" + count); count = right(count, player.score); console.log("after" + count); });
+rbtn.addEventListener('click', function () {  count = right(count, player.score); });
 wbtn1.addEventListener('click', function () { count = wrong(count); })
 wbtn2.addEventListener('click', function () { count = wrong(count); })
 wbtn3.addEventListener('click', function () { count = wrong(count); })
